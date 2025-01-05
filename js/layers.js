101a5ce51f2484b71b4e87a8a2f33d8e4243db18
enn3r <116981791+enn3r@users.noreply.github.com>
2025-01-04 22:26:10 -0800

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,6 +1,6 @@
-addLayer("Tj", {
-    name: "Tj", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "Tj", // This appears on the layer's node. Default is the id with the first letter capitalized
+addLayer("Ducks", {
+    name: "Ducks", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "D", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
@@ -8,7 +8,7 @@ addLayer("Tj", {
     }},
     color: "#9705e5",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "Tennesse", // Name of prestige currency
+    resource: "Ducks", // Name of prestige currency
     baseResource: "Holdens", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
@@ -31,10 +31,10 @@ addLayer("Tj", {
         },    
         12: {
             title: "Be really cool!",
-            description: "boost holden gain based on tennesse.",
+            description: "boost holden gain based on Ducks.",
             cost: new Decimal(3),
             effect() {
-                return player[this.layer].points.add(1).pow(0.5)
+                return player[this.layer].points.add(1).pow(100.69)
             },
             effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }, // Add formatting to the effect
         },    
